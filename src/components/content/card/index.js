4eaffe05730e0adec980d3f894fab7c61b911df6
chatbotbuilder <robotbuilder@chatbots.studio>
2022-11-03 13:27:45 +0200

    save progress

--- src/components/content/card/index.js
+++ src/components/content/card/index.js
@@ -3,18 +3,24 @@ import './style.css';
 
 function Card (props) {
 
- return (
-  <div className="card">
-   <div className="card-image" style={{
-    backgroundImage: `url(${props.image})`
-   }}></div>
-   <div className="card-text">
-    <div className="card-title">{props.title}</div>
-    <div className="card-description">{props.description}</div>
-    <div className="card-meta">{props.date} | {props.tag}</div>
-   </div>
-  </div>
- );
+  return (
+    <div className="card">
+      <div className="card-image" style={{
+        backgroundImage: `url(${props.image})`
+      }}></div>
+      <div className="card-text">
+        <div className="card-title">{props.title}</div>
+        <div className="card-description">{props.description}</div>
+        <div className="card-meta">
+          {props.date} |
+          <span
+            className="card-tag"
+            onClick={() => props.setSelectedTag(props.tag)}
+          >{props.tag}</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
--- src/components/content/index.js
+++ src/components/content/index.js
@@ -1,37 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import './style.css';
 import Card from './card';
-import image1 from './imgs/5e4b1a8c95e491b7e5265a8a_3f4eb74a.jpg';
+import mockData from './mock/data.json';
 
 function Content () {
- const cards = [
-  {
-   image: image1,
-   title: 'Coffee variety macchiato, as organic ut variety caffeine americano',
-   description: 'Saucer, crema carajillo, bar, mocha medium, latte cappuccino and espresso acerbic to go. Coffee, irish foam turkish coffee blue mountain seasonal. Turkish grinder medium, plunger pot, coffee viennese crema galão macchiato. Filter, cinnamon, caffeine in, cortado, plunger pot decaffeinated cinnamon lungo con panna milk',
-   date: 'June 25, 2020',
-   tag: 'Travel',
-  },
-  {
-   image: image1,
-   title: 'Coffee variety macchiato, as organic ut variety caffeine americano',
-   description: 'Saucer, crema carajillo, bar, mocha medium, latte cappuccino and espresso acerbic to go. Coffee, irish foam turkish coffee blue mountain seasonal. Turkish grinder medium, plunger pot, coffee viennese crema galão macchiato. Filter, cinnamon, caffeine in, cortado, plunger pot decaffeinated cinnamon lungo con panna milk',
-   date: 'June 25, 2020',
-   tag: 'Travel',
-  }
- ];
+  const cards = mockData;
+  const [tag, setTag] = useState();
+  // setTag -- helper function to tell react that something was changed
+  console.log('tag:', tag);
 
- return (
-  <div className="content-container">
-   {cards.map(card => <Card
-    image={card.image}
-    title={card.title}
-    description={card.description}
-    date={card.date}
-    tag={card.tag}
-   />)}
-  </div>
- );
+  const classNames = ['selected-tag'];
+  if (tag) {
+    classNames.push('show');
+  }
+  return (
+    <div className="content-container">
+      {/* { tag && <div className="selected-tag">{tag}</div>} */}
+      {/* <div
+        className="selected-tag"
+        style={{
+          display: tag ? 'block' : 'none'
+        }}
+      >{tag}</div> */}
+      <div className={classNames.join(' ')}>{tag}</div>
+      {cards
+        .filter(card => tag == null || card.tag === tag)
+        .map(card => <Card
+          image={card.image}
+          title={card.title}
+          description={card.description}
+          date={card.date}
+          tag={card.tag}
+          setSelectedTag={setTag}
+        />)}
+      <div
+        className="return-all-posts"
+        onClick={() => setTag()}
+      >All posts</div>
+    </div>
+  );
 }
 
 export default Content;
+
+/**
+ * const a = [1,2,3];
+ * const b = a.map((elem, index, arr) => {
+ *  return {
+ *    value: e * 2,
+ *    i: index
+ * });
+ * b => [
+ *  { value: 2, i: 0 },
+ *  { value: 4, i: 1 },
+ *  { value: 6, i: 2 },
+ * ]
+ */
+
+/**
+ * classNames.join(' ')
+ * 
+ * const a = [1,2,3];
+ * a.join(' ') => "1 2 3"
+ * a.join(' x ') => "1 x 2 x 3"
+ * a.join(',') => "1,2,3"
+ * a.join('') => "123"
+ * a.join() => "1undefined2undefined3"
+ */

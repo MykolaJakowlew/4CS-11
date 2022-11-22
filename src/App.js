eda7de3ade78ef84c591c0b7190a9bf940d8b1ae
chatbotbuilder <robotbuilder@chatbots.studio>
2022-11-22 13:15:36 +0200

    save process

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import { useState } from 'react';
 import './App.css';
 import WeatherWidget from './components/weatcher-widget';
+import { SnackbarProvider } from 'react-snackbar-messages';
 function App () {
-  const [cityName, setCityName] = useState();
+  const [cityName, setCityName] = useState('Kyiv');
+  // onFocus - click on element
+  // onBlur - click on other element
+  // click on element => onFocus
+  // click on element => onFocus => click on other element => onBlur
 
-  const onBlur = (event) => {
+  const changeCityName = (event) => {
+    // event.target -- HTML element where event happened
     const value = event.target.value;
     console.log(value);
     setCityName(value);
   };
-
   return (
-    <div className="App">
-      <input onBlur={onBlur} placeholder='City name' />
-      <WeatherWidget city={cityName} />
-    </div>
+    <SnackbarProvider>
+      <div className="App">
+        <div className='city-input'>
+          <input onBlur={changeCityName} />
+        </div>
+        <WeatherWidget city={cityName} />
+      </div>
+    </SnackbarProvider>
   );
 }
 
--- src/components/weatcher-widget/index.js
+++ src/components/weatcher-widget/index.js
@@ -1,77 +1,140 @@
 import React, { useEffect, useState } from "react";
 import './style.css';
-import axios from 'axios';
 // https://openweathermap.org/
 import weatherMock from './weather.mock.json';
-import ModalWindow from "../modal-window";
+import axios from 'axios';
+// https://www.npmjs.com/package/react-loader-spinner
+import { Bars } from 'react-loader-spinner';
+import { SnackbarProvider, useSnackbar } from 'react-snackbar-messages';
+
 
 function WeatherWidget (props) {
- const [hide, setHide] = useState(props.hide);
- const [errorText, setErrorText] = useState();
- const [isError, setIsError] = useState(false);
- const [isLoad, setIsLoad] = useState(false);
- const [weatherData, setWeatherData] = useState();
+  const [isLoad, setIsLoad] = useState(false);
+  const [weatherData, setWeatherData] = useState(weatherMock);
 
- useEffect(() => {
-  setIsLoad(false);
-  if (props.city) {
-   axios.get(
-    `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=6490ea3ba3c851128f51015075087ee2`
-   ).then((response) => {
-    console.log('response:', response);
-    setWeatherData(response.data);
-    setIsLoad(true);
-   }).catch(err => {
-    switch (err.response.status) {
-     case 401: {
-      setErrorText(`Wrong api key was used for this request`);
-      break;
-     }
-     case 400: {
-      setErrorText(`we not found city by name:${props.city}`);
-      break;
-     }
-     case 429: {
-      setErrorText('Too many requests');
-      break;
-     }
-     default: { }
-    }
-    setIsError(true);
-    setHide(false);
-   });
-  }
- }, [props.city]);
+  const snackbar = useSnackbar();
+  // setTimeout(() => { setIsLoad(true); }, 5000);
+  useEffect(() => {
+    setIsLoad(false);
+    axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=6490ea3ba3c851128f51015075087`
+    ).then(response => {
+      console.log(response.data);
+      setWeatherData(response.data);
+      setIsLoad(true);
+    }).catch(err => {
+      console.log(err);
+      switch (err.response.status) {
+        case 401: {
+          snackbar.add('Wrong api key was used', {
+            appearance: 'info',
+            autoDismiss: true,
+            placement: 'top-right'
+          });
+          break;
+        }
+        case 404: {
+          snackbar.add('You set wrong city name', {
+            appearance: 'error',
+            autoDismiss: true,
+            placement: 'top-right'
+          });
+          break;
+        }
+        case 429: {
+          snackbar.add('To many request per second', {
+            appearance: 'info',
+            autoDismiss: true,
+            placement: 'top-right'
+          });
+        }
+        default: { }
+      }
+    });
+  },
+    // componentDidAmount -- first component render
+    []
+    // componentDidUpdate -- each time when dependencies was changed
+    // [props.city]
+  );
 
- return (
-  <div className="weather-widget">
-   {isError &&
-    <ModalWindow
-     style={{ display: hide ? 'none' : 'flex' }}
-     onClick={() => { setHide(true); }}
-    >
-     <div> This is error message: {errorText}</div>
-    </ModalWindow>}
-   {
-    !isLoad
-     ? (<div className="spinner-wrap"><div className="lds-spinner" ><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div></div>)
-     : (
-      <div>
-       <div>City name:{props.city}</div>
-       <div className="row-flex">
-        <div className="weather-widget-coord">
-         <div>Longitude:{weatherData.coord.lon}</div>
-         <div>Latitude:{weatherData.coord.lat}</div>
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  // useEffect(async () => {
+  //   setIsLoad(false);
+  //   try {
+  //     const response = await axios.get(
+  //       `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=6490ea3ba3c851128f51015075087ee2`
+  //     );
+  //     console.log(response.data);
+  //     setWeatherData(response.data);
+  //     setIsLoad(true);
+  //   } catch (err) {
+  //     console.log(err);
+  //     switch (err.response.status) {
+  //       case 401: {
+  //         snackbar.add('Wrong api key was used', {
+  //           appearance: 'info',
+  //           autoDismiss: true,
+  //           placement: 'top-right'
+  //         });
+  //         break;
+  //       }
+  //       case 404: {
+  //         snackbar.add('You set wrong city name', {
+  //           appearance: 'error',
+  //           autoDismiss: true,
+  //           placement: 'top-right'
+  //         });
+  //         break;
+  //       }
+  //       case 429: {
+  //         snackbar.add('To many request per second', {
+  //           appearance: 'info',
+  //           autoDismiss: true,
+  //           placement: 'top-right'
+  //         });
+  //       }
+  //       default: { }
+  //     }
+  //   };
+  // },
+  //   // componentDidAmount -- first component render
+  //   []
+  //   // componentDidUpdate -- each time when dependencies was changed
+  //   // [props.city]
+  // );
+  return (
+    isLoad
+      ? <div className="weather-widget">
+        <div>City name:{props.city}</div>
+        <div className="row-flex">
+          <div className="weather-widget-coord">
+            <div>Longitude:{weatherData.coord.lon}</div>
+            <div>Latitude:{weatherData.coord.lat}</div>
+          </div>
+          <div className="weather-widget-image">
+            {/* <img src={`http://openweathermap.org/img/w/${icon}.png`} alt='' /> */}
+          </div>
         </div>
-        <div className="weather-widget-image">
-         {/* <img src={`http://openweathermap.org/img/w/${icon}.png`} alt='' /> */}
-        </div>
-       </div>
       </div>
-     )
-   }
-  </div>
- );
+      : <Bars />
+
+    // isLoad == false
+    // !isLoad
+    //   ? <Bars />
+    //   : <div className="weather-widget">
+    //     <div>City name:{props.city}</div>
+    //     <div className="row-flex">
+    //       <div className="weather-widget-coord">
+    //         <div>Longitude:{weatherData.coord.lon}</div>
+    //         <div>Latitude:{weatherData.coord.lat}</div>
+    //       </div>
+    //       <div className="weather-widget-image">
+    //         {/* <img src={`http://openweathermap.org/img/w/${icon}.png`} alt='' /> */}
+    //       </div>
+    //     </div>
+    //   </div>
+  );
 }
 
 export default WeatherWidget;
